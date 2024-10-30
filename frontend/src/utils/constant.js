4e95d0f110adc61ddefa886f527eed41662910c2
Oumaima Tayyarat <158475593+OumaimaTayyarat@users.noreply.github.com>
2024-10-30 20:47:12 +0100

    Update constant.js

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,4 +1,4 @@
-export const USER_API_END_POINT = `${import.meta.env.VITE_LOCALHOST}/api/v1/user`;
-export const JOB_API_END_POINT = `${import.meta.env.VITE_LOCALHOST}/api/v1/job`;
-export const APPLICATION_API_END_POINT = `${import.meta.env.VITE_LOCALHOST}/api/v1/application`;
-export const COMPANY_API_END_POINT = `${import.meta.env.VITE_LOCALHOST}/api/v1/company`;
+export const USER_API_END_POINT = "https://thr-app.netlify.app/api/v1/user";
+export const JOB_API_END_POINT = "https://thr-app.netlify.app/api/v1/job";
+export const APPLICATION_API_END_POINT = "https://thr-app.netlify.app/api/v1/application";
+export const COMPANY_API_END_POINT = "https://thr-app.netlify.app/api/v1/company";
