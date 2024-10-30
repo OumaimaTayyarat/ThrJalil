83b27efd2fde51fdd9fe65d9ae9f2cc4f70132f9
Oumaima Tayyarat <158475593+OumaimaTayyarat@users.noreply.github.com>
2024-10-30 20:44:59 +0100

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-  origin:process.env.LOCALHOST,
+  origin:"https://thr-app.netlify.app",
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true,
  
